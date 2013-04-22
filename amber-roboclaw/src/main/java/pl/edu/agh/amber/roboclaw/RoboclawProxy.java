4c6c2f8f2a3ae439124425f0365501726c792d31
Michał Konarski <m4jkel@gmail.com>
2013-04-22 02:12:01 +0200

    Reading motors current speed added.

--- amber-roboclaw/src/main/java/pl/edu/agh/amber/roboclaw/RoboclawProxy.java
+++ amber-roboclaw/src/main/java/pl/edu/agh/amber/roboclaw/RoboclawProxy.java
@@ -1,38 +1,42 @@
 package pl.edu.agh.amber.roboclaw;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
 import pl.edu.agh.amber.common.AmberClient;
 import pl.edu.agh.amber.common.AmberProxy;
+import pl.edu.agh.amber.common.FutureObject;
 import pl.edu.agh.amber.common.proto.CommonProto.DriverHdr;
 import pl.edu.agh.amber.common.proto.CommonProto.DriverMsg;
 import pl.edu.agh.amber.roboclaw.proto.RoboclawProto;
-import pl.edu.agh.amber.roboclaw.proto.RoboclawProto.MotorsQuadCommand;
+import pl.edu.agh.amber.roboclaw.proto.RoboclawProto.MotorsSpeed;
 
 import com.google.protobuf.ExtensionRegistry;
-
 
 public class RoboclawProxy extends AmberProxy {
 
 	private final static int DEVICE_TYPE = 2;
-	
-	private int synNum = 100;	
+
+	private int synNum = 100;
 	private final ExtensionRegistry extensionRegistry;
-	
+
+	private Map<Integer, FutureObject> futureObjectsMap = new ConcurrentHashMap<Integer, FutureObject>();
+
 	public RoboclawProxy(AmberClient amberClient, int deviceID) {
-		super(DEVICE_TYPE, deviceID, amberClient, Logger.getLogger("RoboclawProxy"));
-		
+		super(DEVICE_TYPE, deviceID, amberClient, Logger
+				.getLogger("RoboclawProxy"));
+
 		logger.info("Starting and registering RoboclawProxy.");
-		
+
 		extensionRegistry = ExtensionRegistry.newInstance();
-		RoboclawProto.registerAllExtensions(extensionRegistry);	
+		RoboclawProto.registerAllExtensions(extensionRegistry);
 	}
-	
+
 	synchronized private int getNextSynNum() {
 		return synNum++;
 	}
-	
 
 	@Override
 	public ExtensionRegistry getExtensionRegistry() {
@@ -41,28 +45,83 @@ public class RoboclawProxy extends AmberProxy {
 
 	@Override
 	public void handleDataMsg(DriverHdr header, DriverMsg message) {
+		logger.fine("Handling data message");
+		
+		if (!message.hasAckNum() || message.getAckNum() == 0) {
+
+		} else {
+			int ackNum = message.getAckNum();
+
+			// TODO: automatically removing abandoned futureObjects
+			if (futureObjectsMap.containsKey(ackNum)) {
+				FutureObject futureObject = futureObjectsMap.remove(ackNum);
+				
+				if (futureObject instanceof MotorsCurrentSpeed) {
+					fillMotorsCurrentSpeed((MotorsCurrentSpeed)futureObject, message);
+				}
+			}
+		}
 		
 	}
-	
-	public void sendMotorsCommand(int frontLeftSpeed, int frontRightSpeed, int rearLeftSpeed, int rearRightSpeed) throws IOException {
-		logger.fine(String.format("Sending MotorsCommand: %d %d %d %d.", 
+
+	public void sendMotorsCommand(int frontLeftSpeed, int frontRightSpeed,
+			int rearLeftSpeed, int rearRightSpeed) throws IOException {
+		logger.fine(String.format("Sending MotorsCommand: %d %d %d %d.",
 				frontLeftSpeed, frontRightSpeed, rearLeftSpeed, rearRightSpeed));
-		
+
 		DriverMsg.Builder driverMsgBuilder = DriverMsg.newBuilder();
 		driverMsgBuilder.setType(DriverMsg.MsgType.DATA);
-		
-		MotorsQuadCommand.Builder commandBuilder = MotorsQuadCommand.newBuilder();
+
+		MotorsSpeed.Builder commandBuilder = MotorsSpeed.newBuilder();
 
 		commandBuilder.setFrontLeftSpeed(frontLeftSpeed);
 		commandBuilder.setFrontRightSpeed(frontRightSpeed);
 		commandBuilder.setRearLeftSpeed(rearLeftSpeed);
 		commandBuilder.setRearRightSpeed(rearRightSpeed);
 
-		MotorsQuadCommand motorsQuadCommand = commandBuilder.build();
-		driverMsgBuilder.setExtension(RoboclawProto.motorsCommand, motorsQuadCommand);
+		MotorsSpeed motorsSpeed = commandBuilder.build();
+		driverMsgBuilder.setExtension(RoboclawProto.motorsCommand, motorsSpeed);
 		driverMsgBuilder.setSynNum(getNextSynNum());
-		
+
 		amberClient.sendMessage(buildHeader(), driverMsgBuilder.build());
+	}
+
+	private void fillMotorsCurrentSpeed(MotorsCurrentSpeed mcs,
+			DriverMsg message) {
+		MotorsSpeed inputMcs = message.getExtension(RoboclawProto.currentSpeed);
+
+		mcs.setFrontLeftSpeed(inputMcs.getFrontLeftSpeed());
+		mcs.setFrontRightSpeed(inputMcs.getFrontRightSpeed());
+		mcs.setRearLeftSpeed(inputMcs.getRearLeftSpeed());
+		mcs.setRearRightSpeed(inputMcs.getRearRightSpeed());
+		
+		mcs.setAvailable();
+	}
+
+	private DriverMsg buildCurrentSpeedRequestMsg(int synNum) {
+
+		DriverMsg.Builder driverMsgBuilder = DriverMsg.newBuilder();
+		driverMsgBuilder.setType(DriverMsg.MsgType.DATA);
+		driverMsgBuilder.setExtension(RoboclawProto.currentSpeedRequest, true);
+
+		driverMsgBuilder.setSynNum(synNum);
+
+		return driverMsgBuilder.build();
+	}
+
+	public MotorsCurrentSpeed getCurrentMotorsSpeed() throws IOException {
+		logger.fine("Getting current motors speed.");
+
+		int synNum = getNextSynNum();
+
+		DriverMsg currentSpeedRequestMsg = buildCurrentSpeedRequestMsg(synNum);
+
+		MotorsCurrentSpeed motorsCurrentSpeed = new MotorsCurrentSpeed();
+		futureObjectsMap.put(synNum, motorsCurrentSpeed);
+
+		amberClient.sendMessage(buildHeader(), currentSpeedRequestMsg);
+
+		return motorsCurrentSpeed;
 	}
 
 }
--- examples/roboclaw_example/src/RoboclawExample.java
+++ examples/roboclaw_example/src/RoboclawExample.java
@@ -1,38 +1,43 @@
 import java.io.IOException;
 
 import pl.edu.agh.amber.common.AmberClient;
+import pl.edu.agh.amber.roboclaw.MotorsCurrentSpeed;
 import pl.edu.agh.amber.roboclaw.RoboclawProxy;
 
 public class RoboclawExample {
-	
+
 	public static void main(String[] args) {
 		(new RoboclawExample()).runDemo();
 	}
-	
+
 	public void runDemo() {
 
 		AmberClient client;
 		try {
-			client = new AmberClient("192.168.2.201", 26233);
-			
+			client = new AmberClient("192.168.1.50", 26233);
+
 		} catch (IOException e) {
 			System.out.println("Unable to connect to robot: " + e);
 			return;
 		}
-		
+
 		RoboclawProxy roboclawProxy = new RoboclawProxy(client, 0);
-				
+
 		final int speed = 1000;
-		
 		try {
-			roboclawProxy.sendMotorsCommand(speed, -speed, speed, -speed);
+			roboclawProxy.sendMotorsCommand(speed, speed, speed, speed);
+			MotorsCurrentSpeed mcs = roboclawProxy.getCurrentMotorsSpeed();
+			mcs.waitAvailable();
 			
-			Thread.sleep(10000);
-			
+			System.out.println(String.format(
+				"Motors current speed: fl: %d, fr: %d, rl: %d, rr: %d",				
+				mcs.getFrontLeftSpeed(), mcs.getFrontRightSpeed(),
+				mcs.getRearLeftSpeed(), mcs.getRearRightSpeed()));
+				
 		} catch (IOException e) {
 			System.out.println("Error in sending a command: " + e);
-		} catch (InterruptedException e) {
-			
+		} catch (Exception e) {
+			e.printStackTrace();
 		} finally {
 			client.terminate();
 		}
