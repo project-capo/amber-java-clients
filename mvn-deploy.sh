a5879adbb20449050f07295e05bd6f47205b7b82
Paweł Suder <pawel@suder.info>
2014-10-22 11:53:49 +0200

    Add mvn deploy script, add directories used to mvn deploy to ignored

---
+++ mvn-deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export __dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd ${__dir}
+    __current_branch=$(git branch | grep \* | sed 's/\*\s//g')
+
+    mvn clean
+    mvn deploy
+
+    mkdir -p ${__dir}/repo
+    for path in $(find . -type d -name mvn-repo)
+    do
+        cp --verbose -r ${__dir}/${path}/ ${__dir}/repo/
+    done
+
+    git checkout mvn-repo
+    cp --verbose -r ${__dir}/repo/mvn-repo/pl ${__dir}/
+    git add --all
+    git commit -m "Update mvn-repo"
+    git push origin mvn-repo
+
+    git checkout ${__current_branch}
+popd
