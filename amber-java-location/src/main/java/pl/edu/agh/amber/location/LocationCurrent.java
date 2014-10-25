eeb0ce8f37ee30aa29673b647473783acf2d318b
showmen15 <showmen15@poczta.onet.pl>
2014-10-25 22:05:57 +0200

    Code clean
    tested ready to deploy

--- amber-java-location/src/main/java/pl/edu/agh/amber/location/LocationCurrent.java
+++ amber-java-location/src/main/java/pl/edu/agh/amber/location/LocationCurrent.java
@@ -2,85 +2,84 @@ package pl.edu.agh.amber.location;
 
 import pl.edu.agh.amber.common.FutureObject;
 
-
 public class LocationCurrent extends FutureObject {
 
-    private double X;
-    private double Y;
-    private double angle;
-    private double P;
-    private double timeStamp;
+	private double X;
+	private double Y;
+	private double angle;
+	private double P;
+	private double timeStamp;
 
-    public LocationCurrent() {
+	public LocationCurrent() {
 
-    }
+	}
 
-    public LocationCurrent(double x, double y,
-                              double angle, double p,double timeStamp) {
-        this.X = x;
-        this.Y = y;
-        this.angle = angle;
-        this.P = p;
-        this.timeStamp = timeStamp;
-    }
+	public LocationCurrent(double x, double y, double angle, double p,
+			double timeStamp) {
+		this.X = x;
+		this.Y = y;
+		this.angle = angle;
+		this.P = p;
+		this.timeStamp = timeStamp;
+	}
 
-    public double getX() throws Exception {
-        if (!available) {
-            waitAvailable();
-        }
+	public double getX() throws Exception {
+		if (!available) {
+			waitAvailable();
+		}
 
-        return X;
-    }
+		return X;
+	}
 
-    public void setX(double x) {
-        this.X = x;
-    }
+	public void setX(double x) {
+		this.X = x;
+	}
 
-    public double getY() throws Exception {
-        if (!available) {
-            waitAvailable();
-        }
+	public double getY() throws Exception {
+		if (!available) {
+			waitAvailable();
+		}
 
-        return Y;
-    }
+		return Y;
+	}
 
-    public void setY(double y) {
-        this.Y = y;
-    }
+	public void setY(double y) {
+		this.Y = y;
+	}
 
-    public double getAngle() throws Exception {
-        if (!available) {
-            waitAvailable();
-        }
+	public double getAngle() throws Exception {
+		if (!available) {
+			waitAvailable();
+		}
 
-        return angle;
-    }
+		return angle;
+	}
 
-    public void setAngle(double angle) {
-        this.angle = angle;
-    }
+	public void setAngle(double angle) {
+		this.angle = angle;
+	}
 
-    public double getP() throws Exception {
-        if (!available) {
-            waitAvailable();
-        }
+	public double getP() throws Exception {
+		if (!available) {
+			waitAvailable();
+		}
 
-        return P;
-    }
+		return P;
+	}
 
-    public void setP(double p) {
-        this.P = p;
-    }
-    
-    public double getTimeStamp() throws Exception {
-        if (!available) {
-            waitAvailable();
-        }
+	public void setP(double p) {
+		this.P = p;
+	}
 
-        return timeStamp;
-    }
+	public double getTimeStamp() throws Exception {
+		if (!available) {
+			waitAvailable();
+		}
 
-    public void setTimeStamp(double timeStamp) {
-        this.timeStamp = timeStamp;
-    }
+		return timeStamp;
+	}
+
+	public void setTimeStamp(double timeStamp) {
+		this.timeStamp = timeStamp;
+	}
 }
