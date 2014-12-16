1f3d3d26a81736cb957a240e0e587cccbc01537b
Paweł Suder <pawel@suder.info>
2014-12-16 13:36:01 +0100

    Add configuration and location support.

---
+++ amber-java-drive-to-point/src/main/java/pl/edu/agh/amber/drivetopoint/Configuration.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.amber.drivetopoint;
+
+public class Configuration {
+
+    public double maxSpeed;
+
+    public Configuration(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+}
